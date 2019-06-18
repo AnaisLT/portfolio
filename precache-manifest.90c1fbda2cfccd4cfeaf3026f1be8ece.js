d57b1a502a631ed87babefda64c980ac2ec67622
AnaisLT <trope.anais@gmail.com>
2019-06-18 10:55:39 +0100

    Updates

---
+++ precache-manifest.90c1fbda2cfccd4cfeaf3026f1be8ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd06256cfb0604bea1d441c4f4992ae",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "1eb5132d65cc358a0600",
+    "url": "/My-Portfolio/static/css/main.f85a1b27.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/My-Portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1eb5132d65cc358a0600",
+    "url": "/My-Portfolio/static/js/main.414d2c9a.chunk.js"
+  },
+  {
+    "revision": "b7d4bf292e75006047a4",
+    "url": "/My-Portfolio/static/js/runtime~main.f7066801.js"
+  },
+  {
+    "revision": "7dc59c745407a0b2fc62692b93c82bdb",
+    "url": "/My-Portfolio/static/media/HomePageCover.7dc59c74.jpg"
+  }
+]);
