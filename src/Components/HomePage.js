e611c36f253292c9c24d14c0409a5e53f4c50561
AnaisLT <trope.anais@gmail.com>
2019-07-03 12:36:43 +0100

    Fixed the layout with flex.

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -12,43 +12,45 @@ class Home extends Component {
   render() {
     return (
       <>
-        <div id='content'>
-          <div id='name'>
-            <span class='logo'>
-              <span id="h" class='letter'>H</span>
-              <span id="e" class='letter'>E</span>
-              <span id="l" class='letter'>L</span>
-              <span id="l" class='letter'>L</span>
-              <span id="o" class='letter'>0</span>
-            </span>
-            <span class='logo'>
-              <span id="i" class='letter'>I</span>
-              <span id="apos" class='letter'>'</span>
-              <span id="m" class='letter'>M </span>
-              <span id="a" class='letter'> A</span>
-              <span id="n" class='letter'>N</span>
-              <span id="a" class='letter'>A</span>
-              <span id="i" class='letter'>I</span>
-              <span id="s" class='letter'>S</span>
-            </span>
+        <div id='main-container'>
+          <div id='container1'>
+            <div id='name'>
+              <span class='logo'>
+                <span id="h" class='letter'>H</span>
+                <span id="e" class='letter'>E</span>
+                <span id="l" class='letter'>L</span>
+                <span id="l" class='letter'>L</span>
+                <span id="o" class='letter'>0</span>
+              </span>
+              <span class='logo'>
+                <span id="i" class='letter'>I</span>
+                <span id="apos" class='letter'>'</span>
+                <span id="m" class='letter'>M </span>
+                <span id="a" class='letter'> A</span>
+                <span id="n" class='letter'>N</span>
+                <span id="a" class='letter'>A</span>
+                <span id="i" class='letter'>I</span>
+                <span id="s" class='letter'>S</span>
+              </span>
+            </div>
           </div>
-        </div>
-        <div>
-          <p>I love making ideas come to life through coding.</p>
-        </div>
-        <div>
-          <div id='links'>
-            <Link to='/about'>
-              Let's break the ice
-            </Link>
-            <Link to='/projects'>
-              Check my work
-            </Link>
-            <Link to='/contact'>
-              Say "Hi!"
-            </Link>
+          <div>
+            <p>Create.Learn.Code.Repeat</p>
           </div>
-        </div>
+          <div id='container2'>
+            <div id='links'>
+              <Link to='/about'>
+                Let's break the ice
+            </Link>
+              <Link to='/projects'>
+                Check my work
+            </Link>
+              <Link to='/contact'>
+                Say "Hi!"
+            </Link>
+            </div>
+          </div>
+        </div >
       </>
     );
   }
